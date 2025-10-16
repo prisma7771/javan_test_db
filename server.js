57acf0fd07c1e15dbbc53e1203ae3cc232dbee05
Prisma <prismaputra91@gmail.com>
2025-10-16 20:43:58 +0700

    fix cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import apiRoutes from './api/api_routes.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', apiRoutes);
